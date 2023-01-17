35f0a7e1431ce7f75f57fdd2e142c253c7f94632
Thomas De Lathauwer <t.h.s.delathauwer@students.uu.nl>
2023-01-17 12:19:44 +0100

    added station code

--- Tuesday.py
+++ Tuesday.py
@@ -3,6 +3,12 @@ import json
 with open ('precipitation.json') as file:
     Precipitation = 'precipitation.json' 
 #I opened the Json file and renamed it to the Precipitation variable 
+# GHCND:US1WAKG0038 Seattle Station Code
 
 with open('stations.csv') as file:
     Stations = 'stations.csv'
+
+# I opened the CSV file and attributed the file to a Stations variable
+
+for Rain in Precipitation:
+    
